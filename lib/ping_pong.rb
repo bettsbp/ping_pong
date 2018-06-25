9440c0d9840d0374ab9c489a04320b4ece5b5f2f
Brandon Betts and Nick Brown <brandon@>
2018-06-25 13:03:01 -0700

    finish ping pong test

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,3 +1,20 @@
-def ping_pong(ping)
-
+def ping_pong(number)
+  counter = 0
+  number_array = []
+  until (counter > number)
+    if (counter % 15 === 0) & !(counter === 0)
+      number_array.push("ping pong")
+      counter += 1
+    elsif (counter % 3 === 0) & !(counter === 0)
+      number_array.push("ping")
+      counter += 1
+    elsif (counter % 5 === 0) & !(counter === 0)
+      number_array.push("pong")
+      counter += 1
+    else
+      number_array.push(counter)
+      counter += 1
+    end
+  end
+  number_array
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,15 @@ describe('#ping_pong') do
   it ("returns a array of numbers") do
     expect(ping_pong(2)).to(eq([0,1,2]))
   end
+
+  it ("tests if a number is divisible by 3 and replaces the number with the word ping") do
+    expect(ping_pong(3)).to(eq([0,1,2,"ping"]))
+  end
+
+  it ("tests if a number is divisible by 5 and replaces the number with the word pong") do
+    expect(ping_pong(5)).to(eq([0,1,2,"ping",4,"pong"]))
+  end
+  it ("tests if a number is divisible by 15 and replaces the number with the words ping pong") do
+    expect(ping_pong(15)).to(eq([0,1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping pong"]))
+  end
 end
